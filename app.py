59eb08212ff9263ac5baf08319dc39919e9a8910
Elonnn <yilangxu@usc.edu>
2019-12-03 22:35:10 -0800

    add description and about page

--- app.py
+++ app.py
@@ -8,8 +8,15 @@ app = Flask(__name__)
 def basic():
     response = requests.get(url='https://web-project-59451.firebaseio.com/suicide-statistics.json')
     # response.json() is a list
-    return render_template("display.html", data=response.json())
+    return render_template("table.html", data=response.json())
 
+@app.route('/description')
+def description():
+    return render_template("description.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 @app.route('/filterOnNum/<attribute>/<start>/<end>')
 def filterOnNum(attribute: str, start: str, end: str):
@@ -18,7 +25,7 @@ def filterOnNum(attribute: str, start: str, end: str):
     # 'https://web-project-59451.firebaseio.com/suicide-statistics.json?orderBy="year"&startAt=2009&endAt=2010'
    
     # response.json() is a dict
-    return render_template("display.html", data=response.json().values())
+    return render_template("table.html", data=response.json().values())
 
 @app.route('/filterOnCountry/<country>')
 def filterOnCountry(country: str):
@@ -35,7 +42,7 @@ def filterOnCountry(country: str):
     
     res = [data[i] for i in response.json()]
 
-    return render_template("display.html", data=res)
+    return render_template("table.html", data=res)
 
 
 
@@ -45,7 +52,7 @@ def sort(attribute: str, reverse: str):
 
     # data is a list of dict
     data.sort(key=lambda entry: entry[attribute], reverse=(reverse == 'True'))
-    return render_template("display.html", data=data)
+    return render_template("table.html", data=data)
 
 
 if __name__ == '__main__':
